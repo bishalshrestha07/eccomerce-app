d724d7076b2e9b40cf6a79573097d49d0e031841
bishalshrestha07 <biscro7@gmail.com>
2022-01-27 21:09:17 +0545

    worked with backend authorization with jwt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 function App() {
   return (
     <Router>
@@ -12,7 +13,11 @@ function App() {
         <Container>
           <Routes>
             <Route path='/' element={ <HomeScreen /> } exact />
+
             <Route path='/product/:id' element={<ProductScreen />} />
+
+            <Route path='/cart/:id' element={<CartScreen />} />
+            <Route path='/cart/' element={<CartScreen />} />
           </Routes>
           
           
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,30 +1,40 @@
 import React, {useEffect, useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux'
 import { Link,useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import {listProductDetails} from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 
 function ProductScreen() {
+  const [qty, setQty] = useState(1)
   const {id}= useParams()
-  const [product, setProduct ] = useState([])
+  const dispatch = useDispatch()
+  const productDetails = useSelector(state => state.productDetails)
+  const {loading, error, product} = productDetails
+  const navigate = useNavigate();
   
   useEffect(() =>{
+    dispatch(listProductDetails(id))
+    
+  },[dispatch])
 
-    async function fetchProduct(){
-      const { data } = await axios.get(`/api/products/${id}`)
-      setProduct(data)
-    }
-
-    fetchProduct()
-  },[])
+  const addToCartHandler = () => {
+    navigate(`/cart/${id}?qty=${qty}`)
+  }
 
   
   
   return (
     <div>
       <Link to='/' className='btn btn-light my-3'>Go Back</Link>
-      <Row>
+
+      {error ? <Message variant='danger'>{error}</Message>
+      : loading ? <Loader />:
+      (<Row>
         <Col md={6}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -66,13 +76,40 @@ function ProductScreen() {
                 </Row>
               </ListGroup.Item>
 
+              {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Qty</Col>
+                    <Col xs='auto' className='my-1'>
+                      <Form.Control
+                        as="select"
+                        value={qty}
+                        onChange={(e) => setQty(e.target.value)}
+                      >
+                        {
+                          [...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x+1} value={x+1}>
+                              {x+1}
+                            </option>
+                          ))
+                        }
+
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
+
               <ListGroup.Item>
-                <Button className='btn-block' disabled={product.countInStock == 0} type='button'>Add to Cart</Button>
+                <Button 
+                onClick={addToCartHandler}
+                className='btn-block' disabled={product.countInStock == 0} type='button'>Add to Cart</Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
-      </Row>
+      </Row>)}
+      
     </div>
   )
 }
