c3dddfd01c5cfa33947532629c6d5779d68abc68
bishalshrestha07 <biscro7@gmail.com>
2022-01-28 21:23:31 +0545

    worked with login and registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 function App() {
   return (
     <Router>
@@ -13,6 +15,10 @@ function App() {
         <Container>
           <Routes>
             <Route path='/' element={ <HomeScreen /> } exact />
+
+            <Route path='/login' element={ <LoginScreen /> } />
+
+            <Route path='/register' element={ <RegisterScreen /> } />
 
             <Route path='/product/:id' element={<ProductScreen />} />
 
