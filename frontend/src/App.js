38041f21a18119a83d9378608ae8df4a5a315d7c
bishalshrestha07 <biscro7@gmail.com>
2022-02-04 12:20:06 +0545

    completed frontend and backend user authentication functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 function App() {
   return (
     <Router>
@@ -19,6 +20,8 @@ function App() {
             <Route path='/login' element={ <LoginScreen /> } />
 
             <Route path='/register' element={ <RegisterScreen /> } />
+
+            <Route path='/profile' element={ <ProfileScreen /> } />
 
             <Route path='/product/:id' element={<ProductScreen />} />
 
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -11,6 +11,12 @@ import {
   USER_DETAILS_REQUEST, 
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_DETAILS_RESET,
+
+  USER_UPDATE_PROFILE_REQUEST, 
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_RESET,
 } from '../constants/userConstants';
 
 
@@ -25,7 +31,7 @@ export const userLoginReducer = (state = { }, action) => {
       return {loading: false, userInfo : action.payload }
 
     case USER_LOGIN_FAIL:
-      return {loading: true, error : action.payload}
+      return {loading: false, error : action.payload}
 
     case USER_LOGOUT:
       return { }
@@ -44,7 +50,7 @@ export const userRegisterReducer = (state = { }, action) => {
       return {loading: false, userInfo : action.payload }
 
     case USER_REGISTER_FAIL:
-      return {loading: true, error : action.payload}
+      return {loading: false, error : action.payload}
 
     case USER_LOGOUT:
       return { }
@@ -64,10 +70,29 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
       return {loading: false, user : action.payload }
 
     case USER_DETAILS_FAIL:
-      return {loading: true, error : action.payload}
+      return {loading: false, error : action.payload}
 
-   
-    
+    case USER_DETAILS_RESET:
+      return {user: {}}
+    default:
+      return state
+  }
+}
+
+export const userUpdateProfileReducer = (state = { }, action) => {
+  switch(action.type){
+    case USER_UPDATE_PROFILE_REQUEST:
+      return {loading: true}
+
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return {loading: false, success:true, userInfo : action.payload }
+
+    case USER_UPDATE_PROFILE_FAIL:
+      return {loading: false, error : action.payload}
+
+    case USER_UPDATE_PROFILE_RESET:
+      return {}
+      
     default:
       return state
   }
